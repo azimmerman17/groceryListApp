405312332c2012283444275389a2cffe26c06378
Andrew Zimmerman <azimmerman@pga.com>
2023-04-20 19:12:26 -0400

    Added skelton for project

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+const cors = require('cors')
+
+// middleware
+app.set('view engine', 'jsx')
+app.use(express.json())
+app.use(cors())
+
+// controllers and routes
+
+
+// Mongo Connection for future use when need to connect
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+//     .then(() => console.log('DB connected'))
+//     .catch(err => console.error(err));
+
+// Configue Port and listen
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, console.log(`Listening on Port ${PORT}`))
+
+module.exports = app;
