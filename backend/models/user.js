b670cbd13f7dceaf53e54857d846b7cda12f545d
Andrew Zimmerman <azimmerman@pga.com>
2023-04-20 20:09:51 -0400

    Added food schema to the backend models

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,14 +3,17 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   first_name: { 
     type: String,
+    maxLength: 20,
     requred: true,
   },
   last_name: {
     type: String,
+    maxLength: 20,
     required: true,
   },
   email: {
     type: String,
+    maxLength: 100,
     required: true,
   },
   grocery_list: [{
