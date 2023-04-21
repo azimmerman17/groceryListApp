bd3a843ee9614e27b1fa836a11ec47d8f9035858
hauprichth <hauprichth@gmail.com>
2023-04-20 20:29:50 -0400

    added mongo connection

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,15 @@ app.set('view engine', 'jsx')
 app.use(express.json())
 app.use(cors())
 
+
 // controllers and routes
 
 
-// Mongo Connection for future use when need to connect
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
-//     .then(() => console.log('DB connected'))
-//     .catch(err => console.error(err));
+//d db connection
+mongoose.set('strictQuery',true);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
 
 // Configue Port and listen
 const PORT = process.env.PORT || 8080
