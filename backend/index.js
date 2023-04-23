24ba875e4efc9c29293f0f57cb0b3d7089419c5a
Andrew Zimmerman <azimmerman@pga.com>
2023-04-23 12:18:58 -0400

    added user and auth controllers

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,13 @@ app.use(cors())
 
 
 // controllers and routes
+const userRoutes = require('./controllers/user')
+const authRoutes = require('./controllers/authentication')
 
+app.use('/user', userRoutes)
+app.use('/auth', authRoutes)
 
-
-//d db connection
+//db connection
 mongoose.set('strictQuery',true);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('DB connected'))
