2f8dbe28366f33c467cb6f7a73a47fff11505928
Andrew Zimmerman <azimmerman@pga.com>
2023-04-23 11:19:14 -0400

    Added Frontend furm to add a food to db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './Components/NavBar'
-import AddUser from './Components/AddUser'
+import NavBar from './components/NavBar'
+import AddUser from './components/addUser'
+import AddFood from './components/AddFood';
 
 
 function App() {
@@ -16,7 +17,9 @@ function App() {
         <Routes>
           {/* <Route exact path='/' element={} /> */}
           <Route path='/user/addUser' element={<AddUser />} /> 
-          {/* <Route path='/food' element={} /> */}
+          {/* <Route path='/foods' element={} /> */}
+          <Route path='/foods/new' element={<AddFood />} />
+          {/* <Route path='/grocerylist' element={} */}
         </Routes>
       </Router>
     </div>
--- frontend/src/components/addUser.js
+++ frontend/src/components/addUser.js
@@ -4,22 +4,23 @@ import Form from 'react-bootstrap/Form';
 const AddUser = () => {
   return (
     <Form>
-      <Form.Group className="mb-3">
-        <Form.Label htmlFor="first-name-new-user">First Name:</Form.Label>
-          <Form.Control id="first-name-new-user" placeholder="Enter your First Name" />
+      <Form.Group className='mb-3'>
+        <Form.Label htmlFor='first-name-new-user'>First Name:</Form.Label>
+          <Form.Control id='first-name-new-user' placeholder='Enter your First Name' />
       </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label htmlFor="last-name-new-user">Last Name:</Form.Label>
-        <Form.Control id="last-name-new-user" placeholder="Enter your Last Name" />
+      <Form.Group className='mb-3'>
+        <Form.Label htmlFor='last-name-new-user'>Last Name:</Form.Label>
+        <Form.Control id='last-name-new-user' placeholder='Enter your Last Name' />
       </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label htmlFor="email-new-user">Email:</Form.Label>
-        <Form.Control id="email-new-user" placeholder="Enter your email" />
+      <Form.Group className='mb-3'>
+        <Form.Label htmlFor='email-new-user'>Email:</Form.Label>
+        <Form.Control id='email-new-user' placeholder='Enter your email' />
       </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label htmlFor="password-new-user">Password:</Form.Label>
-        <Form.Control id="password-new-user" placeholder="Enter your password" />
+      <Form.Group className='mb-3'>
+        <Form.Label htmlFor='password-new-user'>Password:</Form.Label>
+        <Form.Control id='password-new-user' placeholder='Enter your password' />
       </Form.Group>
+      <Button type="submit">Submit</Button>
     </Form>
   );
 }
