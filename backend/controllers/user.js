55ca833cdf8d595cbae7d7ae0812ecb1c0b13e8f
Andrew Zimmerman <azimmerman@pga.com>
2023-04-27 19:58:03 -0400

    Added auth using jwt

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -14,6 +14,7 @@ router.get('/', async (req, res) => {
 
 // POST
 router.post('/', async (req, res) => {
+  console.log(req.body)
   let { password, ...rest } = req.body
 
   const user = await User.create({
--- frontend/src/Contexts/currentUser.js
+++ frontend/src/Contexts/currentUser.js
@@ -1,14 +1,26 @@
-import { createContext, useState } from "react";
+import { useEffect, createContext, useState } from "react";
 
 export const CurrentUser = createContext()
 
-function CurrentUserProvider({ children }){
+function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
 
+    useEffect(() => {
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:8080/auth/profile', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
+
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-          {console.log(currentUser)}
             {children}
         </CurrentUser.Provider>
     )
